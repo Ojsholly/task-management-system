9e6ab3282401e8d540ca2f416216554211e3935d
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2024-07-26 14:58:15 +0100

    Implemented the unit tests for user authentication.

---
+++ tests/Unit/AuthServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\UserService;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class AuthServiceTest extends TestCase
+{
+    private $userServiceMock;
+
+    private $authService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->userServiceMock = Mockery::mock(UserService::class);
+    }
+
+    public function testLoginFailureDueToUserNotFound()
+    {
+        $this->userServiceMock
+            ->shouldReceive('getUserByField')
+            ->with('email', 'nonexistent@example.com')
+            ->andReturn(null);
+
+        $result = $this->authService->login('nonexistent@example.com', 'password123', 'device-name');
+
+        $this->assertNull($result);
+    }
+}
